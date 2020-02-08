01eb1df46d8113e856001357e048880d816dee62
kbj2060 <kbj2060@naver.com>
2020-02-08 19:50:44 +0900

    a

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PrimarySearchAppBar from './components/PrimarySearchAppBar';
+import { fade, makeStyles, withStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+
+}));
+
+class App extends React.Component {
+  render(){
+    return (
+      <PrimarySearchAppBar />
+    )
+  }
+}
+export default App;
