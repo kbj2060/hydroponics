dc116fc3012de66fb2339e107005bdfac5ea2298
kbj2060 <kbj2060@naver.com>
2020-08-11 12:33:02 +0900

    Menu update

--- client/src/components/AppBar/Menu.js
+++ client/src/components/AppBar/Menu.js
@@ -5,18 +5,53 @@ import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+
+const LinkButton = (props) => {
+	return (
+		<Link to={`/${props.page === 'logout'?'':props.page}`}>
+			<button onClick={props.onClick} className={props.buttonDesign} type="button">
+				<h2>{props.page.toUpperCase()}</h2>
+			</button>
+		</Link>
+	)
+}
+
 
 const useStyles = makeStyles((theme) => ({
 	modal: {
 		display: 'flex',
 		alignItems: 'center',
 		justifyContent: 'center',
+		margin : 'auto',
+		width : '300px',
+		height : '300px',
+		borderBottom : '1px',
 	},
 	paper: {
-		backgroundColor: theme.palette.background.paper,
-		border: '2px solid #000',
-		boxShadow: theme.shadows[5],
-		padding: theme.spacing(2, 4, 3),
+		backgroundColor : 'rgba(255, 255, 255, 0)',
+		borderRadius : '10px',
+		color: 'white',
+	},
+	menuButton : {
+		backgroundColor : 'rgba(255, 255, 255, 0)',
+		border : 'none',
+		width : '100%',
+		color: 'white',
+		textDecoration: 'none',
+		outline : 'none',
+		cursor : 'pointer',
+		'&:hover' : { color : '#FFCB3A'},
+		'&:active' : { transform : 'translateY(2px)'}
+	},
+	header : {
+		borderBottom : '1px solid',
+		paddingBottom : '18.7px',
+		marginLeft : '30px',
+		marginRight : '30px',
+		textAlign : 'center'
 	},
 }));
 
@@ -25,7 +60,7 @@ export default function TransitionsModal() {
 	const [open, setOpen] = React.useState(false);
 
 	const handleOpen = () => {
-		setOpen(true);
+		setOpen((prev) => !prev);
 	};
 
 	const handleClose = () => {
@@ -35,27 +70,27 @@ export default function TransitionsModal() {
 	return (
 		<ClickAwayListener onClickAway={handleClose}>
 		<div>
-			<button type="button" onClick={handleOpen}>
-				react-transition-group
-			</button>
+			<IconButton
+				onClick={handleOpen}
+				type="button"
+				aria-label="show more"
+				aria-haspopup="true">
+				<MenuIcon style={{ color: 'white' }} />
+			</IconButton>
 			<Modal
-				aria-labelledby="transition-modal-title"
-				aria-describedby="transition-modal-description"
 				className={classes.modal}
 				open={open}
-				onClose={handleClose}
-				closeAfterTransition
 				BackdropComponent={Backdrop}
 				BackdropProps={{
 					timeout: 500,
 				}}
 			>
 				<Fade in={open}>
-					<div className={classes.popupWrapper}>
+					<div className={classes.paper}>
 						<h1 className={classes.header}>SMART FARM</h1>
-						<Link  to="/dashboard"><h2>DASHBOARD</h2> </Link>
-						<Link  to="/settings"><h2>SETTING</h2></Link>
-						<Link to="/">LOGOUT</Link>
+						<LinkButton onClick={handleClose} page={"dashboard"} buttonDesign={classes.menuButton} />
+						<LinkButton onClick={handleClose} page={"settings"} buttonDesign={classes.menuButton} />
+						<LinkButton onClick={handleClose} page={"logout"} buttonDesign={classes.menuButton} />
 					</div>
 				</Fade>
 			</Modal>
