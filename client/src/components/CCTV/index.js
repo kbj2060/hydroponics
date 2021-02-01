d5e0f51412f5cfe31ab2666bedcab8cade37b3cd
llewyn <kbj2060@naver.com>
2021-02-02 03:20:36 +0900

    [Security] external ip exposed

--- client/src/components/CCTV/index.js
+++ client/src/components/CCTV/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 export default function CCTV () {
+    console.log(process.cwd())
 	return (
 		<img alt={"cctv"} style={{margin:'auto', minHeight: '300px',   borderRadius: '20px',
 			background: '#161717',
 			boxShadow:  'inset 6px 6px 12px #0b0b0b,inset -6px -6px 12px #212323',}}
-				 src="http://121.157.207.47:9999/?action=stream"
+				 src={process.env.PUBLIC_URL + '/sample.png'}
 		     width='100%' height='100%' muted autoPlay={true} />
 	)
 }
