487f513df23fea222c853bee2e846b6f99a68530
llewyn <kbj2060@naver.com>
2021-02-01 17:29:38 +0900

    [Security] Remove IP Address

--- client/src/components/CCTV/index.js
+++ client/src/components/CCTV/index.js
@@ -5,7 +5,7 @@ export default function CCTV () {
 		<img alt={"cctv"} style={{margin:'auto', minHeight: '300px',   borderRadius: '20px',
 			background: '#161717',
 			boxShadow:  'inset 6px 6px 12px #0b0b0b,inset -6px -6px 12px #212323',}}
-				 src="http://121.157.207.47:9999/?action=stream"
+				 src="CCTV IP ADDRESS"
 		     width='100%' height='100%' muted autoPlay={true} />
 	)
 }
