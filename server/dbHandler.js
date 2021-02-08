8934deda1ca13a3ff23d3b9018e176480a582765
llewyn <kbj2060@naver.com>
2021-02-09 01:52:02 +0900

    [Chore] Edit heroku config var and add to dbHandler.js

--- server/dbHandler.js
+++ server/dbHandler.js
@@ -1,8 +1,8 @@
-const DB_CONF_PATH = "./values/db_conf.json";
+//const DB_CONF_PATH = "./values/db_conf.json";
 
-const fs = require('fs')
-const data = fs.readFileSync(DB_CONF_PATH)
-const conf = JSON.parse(data)
+//const fs = require('fs')
+//const data = fs.readFileSync(DB_CONF_PATH)
+//const conf = JSON.parse(data)
 const mysql = require('mysql');
 const connection = mysql.createConnection({
 		host: process.env.host,
