31c2bdadba063563e413857c195ce0e39b015840
llewyn <kbj2060@naver.com>
2021-02-08 20:15:57 +0900

    [Bug Fix] Git Merge Error

--- client/src/components/CCTV/index.js
+++ client/src/components/CCTV/index.js
@@ -5,7 +5,6 @@ export default function CCTV () {
 		<img alt={"cctv"} style={{margin:'auto', minHeight: '300px',   borderRadius: '20px',
 			background: '#161717',
 			boxShadow:  'inset 6px 6px 12px #0b0b0b,inset -6px -6px 12px #212323',}}
-				 src="CCTV IP ADDRESS"
-				 src={process.env.PUBLIC_URL + '/sample.png'}
-		     width='100%' height='100%' muted autoPlay={true} />
-}	)	)}
+				 src="http://121.157.207.47:9999/?action=stream" width='100%' height='100%' muted autoPlay={true} />
+	)
+}
